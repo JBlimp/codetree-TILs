42475d3b23a2d5c5063ed7210796aa68a5597a57
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-06 11:04:45 +0900

    Title: 데이터 교환, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240306/데이터 교환/exchange-data.c
+++ 240306/데이터 교환/exchange-data.c
@@ -4,12 +4,12 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a = 5, b = 6, c = 7;
     int temp = b;
-    b = a;
-    a = temp;
+    b = c;
+    c = temp;
 
     temp = c;
-    c = b;
-    b = temp;
+    c = a;
+    a = temp;
 
     printf("%d\n%d\n%d\n", a, b, c);
     return 0;
