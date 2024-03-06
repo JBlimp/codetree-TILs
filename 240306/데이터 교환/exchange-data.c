61b772a719b0f489089a4c6abf70c833a73c1f40
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-06 11:07:18 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240306/데이터 교환/exchange-data.c
+++ 240306/데이터 교환/exchange-data.c
@@ -3,14 +3,14 @@
 int main() {
     // 여기에 코드를 작성해주세요.
     int a = 5, b = 6, c = 7;
-    int temp = b;
-    b = a;
-    a = temp;
+    int temp = a;
+    a = c;
+    c = temp;
 
-    temp = c;
-    c = b;
-    a = temp;
+    temp = b;
+    b = c;
+    c = temp;
 
-    printf("%d\n%d\n%d\n", a, b, c);
+    printf("%d\n%d\n%d", a, b, c);
     return 0;
 }
