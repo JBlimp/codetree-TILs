a7b3a53bf8bc9771b59d44f390a28dbeaf25a5bc
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-06 10:56:18 +0900

    Title: 길이 단위 변환하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240306/길이 단위 변환하기/change-length-unit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double oneft = 30.48;
+    double onemi = 160934;
+    double a = 9.2;
+    double b = 1.3;
+    printf("%.1lfft = %.1lfcm", a, a*oneft);
+    printf("%.1lfmi = %.1lfcm", b, b*onemi);
+    return 0;
+}
