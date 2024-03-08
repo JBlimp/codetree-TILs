6955419b17f11bb3596a48d749f009d29f65b60d
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-08 09:59:35 +0900

    Title: 두 번의 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240308/두 번의 연산/two-operations.c
+++ 240308/두 번의 연산/two-operations.c
@@ -4,14 +4,15 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a;
     scanf("%d", &a);
-    if (a%2 == 1) {
+
+    if (a % 2 != 0) {
         a += 3;
     }
 
-    if (a%3 == 0) {
+    if (a % 3 == 0) {
         a /= 3;
     }
 
-    printf("&d", a);
+    printf("%d", a);
     return 0;
 }
