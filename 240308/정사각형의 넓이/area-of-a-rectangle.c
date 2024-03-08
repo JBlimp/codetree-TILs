fbc64c2f835a02bdfbc3dc08ccbd9704a7553f78
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-08 09:27:46 +0900

    Title: 정사각형의 넓이, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240308/정사각형의 넓이/area-of-a-rectangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    scanf("%d", &a);
+    printf("%d\n", a * a);
+    if ( a < 5) {
+        printf("tiny");
+    }
+    return 0;
+}
