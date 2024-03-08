025b37b865e817203204525f3a845aa33c28a74d
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-08 09:54:11 +0900

    Title: 최대 2번의 연산, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240308/최대 2번의 연산/up-to-2-calculations.c
+++ 240308/최대 2번의 연산/up-to-2-calculations.c
@@ -4,7 +4,12 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a;
     scanf("%d", &a);
-    
+
+    if (a == 1) {
+        printf("1");
+        return 0;
+    }
+
     if (a/2 == 0) {
         a /= 2;
     }
