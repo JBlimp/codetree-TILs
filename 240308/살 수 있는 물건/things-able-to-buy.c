d425397114973422f354e29764c8c4f86b9fb8c0
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-08 09:40:44 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240308/살 수 있는 물건/things-able-to-buy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    scanf("%d", &a);
+    if (a >= 3000) {
+        printf("book");
+    } else if ( a >= 1000) {
+        printf("mask");
+    } else {
+        printf("no");
+    }
+    return 0;
+}
