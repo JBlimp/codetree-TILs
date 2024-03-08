b51afba9323fdcd4332a1e2eb0da250940f69998
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-08 09:34:31 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240308/삼항연산자/ternary-operator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    scanf("%d", &a);
+    int grade = a == 100 ? 0 : 1;
+    if (grade == 0) {
+        printf("pass");
+    } else {
+        printf("fail");
+    }
+    return 0;
+}
