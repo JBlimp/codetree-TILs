6d5b459fb86431f7693c1991b9cab86753a3cb64
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-08 09:24:06 +0900

    Title: 체질량지수, Time: 14ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240308/체질량지수/bmi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int h, w;
+    scanf("%d %d", h, w);
+    int bmi = h / (w * w);
+    printf("%d\n", bmi);
+    if (bmi >= 25) {
+        printf("Obesity");
+    }
+    return 0;
+}
