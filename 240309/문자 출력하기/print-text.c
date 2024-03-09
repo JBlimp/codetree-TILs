b76a2dffcfea5accb43e60b186b4586aec0eaeb8
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-09 21:56:18 +0900

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240309/문자 출력하기/print-text.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char input;
+    scanf("%c", &input);
+
+    for(int i = 0; i < 8; i++) {
+        printf("%c", input);
+    }
+    return 0;
+}
