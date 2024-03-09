947912df267f527128e68945b73f12a948f4c117
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-09 20:48:32 +0900

    Title: b부터 a까지 감소, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240309/b부터 a까지 감소/dec-from-b-to-a.c
+++ 240309/b부터 a까지 감소/dec-from-b-to-a.c
@@ -3,9 +3,9 @@
 int main() {
     // 여기에 코드를 작성해주세요.
     int a, b;
-    scanf("%d %d", a, b);
+    scanf("%d %d", &a, &b);
     for(;b >= a; b--) {
-        printf("%d", b);
+        printf("%d ", b);
     }
     return 0;
 }
