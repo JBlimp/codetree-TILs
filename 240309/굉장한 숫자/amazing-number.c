a841c3d7b707591e12859300576edb05be6ca66d
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-09 19:53:54 +0900

    Title: 굉장한 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240309/굉장한 숫자/amazing-number.c
+++ 240309/굉장한 숫자/amazing-number.c
@@ -5,7 +5,7 @@ int main() {
     int a;
     scanf("%d", &a);
 
-    if ((a%2 == 1 && a%3 == 0) || (a%2 == 0 || a%5 == 0)) {
+    if ((a%2 == 1 && a%3 == 0) || (a%2 == 0 && a%5 == 0)) {
         printf("true");
     } else {
         printf("false");
