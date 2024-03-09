7f01f2f1a286be9a0fa4196ec09156eac0e459f9
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-09 20:11:21 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240309/윤년인가/is-leap-year.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int year;
+    scanf("%d", &year);
+
+    if ((year%4 == 0) && !(year%100 == 0 && year&400 != 0)) {
+        printf("true");
+    } else {
+        printf("false");
+    }
+    return 0;
+}
