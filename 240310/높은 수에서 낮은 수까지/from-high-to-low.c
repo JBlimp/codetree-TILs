7a332395898207bc0bbb2264b8b19371ea6dec8f
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-10 14:42:10 +0900

    Title: 높은 수에서 낮은 수까지, Time: 3ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240310/높은 수에서 낮은 수까지/from-high-to-low.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    scanf("%d %d", &b, &a);
+
+    for (; b >= a; b--) {
+        printf("%d ", b);
+    }
+    return 0;
+}
