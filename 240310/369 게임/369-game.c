f699a0abf7475ad495ec75da9b505f45e30644a9
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-10 14:59:55 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240310/369 게임/369-game.c
+++ 240310/369 게임/369-game.c
@@ -7,7 +7,7 @@ int main() {
 
     for (int i = 1; i <= n; i++) {
         if (i%3 == 0 || (i/10)%3 == 0 || (i/100)%3 == 0) {
-            printf("%0 ");
+            printf("0 ");
         } else {
             printf("%d ", i);
         }
