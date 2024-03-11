ff255d4896626cb38e959a6da354155404eb9eea
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-12 08:54:52 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240312/369 게임/369-game.c
+++ 240312/369 게임/369-game.c
@@ -6,18 +6,23 @@ int main() {
     scanf("%d", &n);
 
     for (int i = 1; i <= n; i++) {
-        int flag = 0;
         int num = i;
-        while (num != 0) {
+        int has_369 = 0; // 3, 6, 9가 있는지 여부를 나타내는 플래그
+
+        // 숫자의 각 자릿수를 확인하여 3, 6, 9가 있는지 확인
+        while (num > 0) {
             int digit = num % 10;
             if (digit == 3 || digit == 6 || digit == 9) {
-                printf("0 ");
-                flag = 1;
+                has_369 = 1;
                 break;
             }
             num /= 10;
         }
-        if (flag == 0) {
+
+        // 3의 배수인 경우 또는 3, 6, 9가 있는 경우 0을 출력
+        if (i % 3 == 0 || has_369) {
+            printf("0 ");
+        } else {
             printf("%d ", i);
         }
     }
