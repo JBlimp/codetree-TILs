d5a11783cebd9fceddf0c8412c693c6c0cb895ad
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-12 09:15:19 +0900

    Title: 윤년 횟수 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240312/윤년 횟수 구하기/number-of-leap-years.c
+++ 240312/윤년 횟수 구하기/number-of-leap-years.c
@@ -5,8 +5,11 @@ int main() {
     int n, cnt = 0;
     scanf("%d", &n);
     for (int i = 1; i <= n; i++) {
-        if (n%4 == 0 && !(n%100 == 0 && n%400 != 0)) {
+        if (n%4 == 0) {
             cnt++;
+            if (n%100 == 0 && n%400 != 0) {
+                cnt--;
+            }
         }
     }
 
