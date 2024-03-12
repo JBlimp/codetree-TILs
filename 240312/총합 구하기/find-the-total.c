a4e16e3271d071875481604c4dc26eb1b6798e17
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-12 10:39:58 +0900

    Title: 총합 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240312/총합 구하기/find-the-total.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, sum = 0;
+    scanf("%d %d", &a, &b);
+
+    for (; a <= b; a++) {
+        if (a % 6 == 0 && a % 8 != 0) {
+            sum += a;
+        }
+    }
+
+    printf("%d", sum);
+    return 0;
+}
