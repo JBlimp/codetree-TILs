f24aa6a83ce680c530bfbb8a582f86dc70cafea1
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-12 10:27:53 +0900

    Title: 숫자 사이의 배수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240312/숫자 사이의 배수/multiplication-between-numbers.c
+++ 240312/숫자 사이의 배수/multiplication-between-numbers.c
@@ -2,16 +2,19 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a, b, sum = 0;
+    int a, b, sum = 0, cnt = 0;
     float ave;
     scanf("%d %d", &a, &b);
 
-    for (int i = a; i <= b; i++) {
-        sum += a;
+    for (; a <= b; a++) {
+        if (a % 5 == 0 || a % 7 == 0) {
+            cnt += 1;
+            sum += a;
+        }
     }
 
-    ave = sum / (b - a);
+    ave = sum / cnt;
 
-    printf("%.1f", ave);
+    printf("%d %.1f",sum, ave);
     return 0;
 }
