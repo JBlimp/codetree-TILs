a4da65cef501afdf4d2953d0385d1da96aa01626
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-12 16:36:28 +0900

    Title: 몫 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240312/몫 출력하기/print-share.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, cnt = 0;
+    while (1) {
+        scanf("%d", &n);
+        if (n % 2 == 0) {
+            printf("%d\n", n / 2);
+            cnt++;
+        }
+        if (cnt >= 3) {
+            break;
+        }
+    }
+    return 0;
+}
