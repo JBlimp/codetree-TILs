b2d8f393cc029ea083f818299bf2980d2b7e7c86
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-12 09:09:46 +0900

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240312/청소당번 정하기/cleaning-numbering.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+
+    int cnt_class = 0, cnt_hall = 0, cnt_bath = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (i % 12 == 0) {
+            cnt_bath++;
+        } else if (i % 6 == 0 || i % 3 == 0) {
+            cnt_hall++;
+        } else if (i % 2 == 0) {
+            cnt_class++;
+        }
+    }
+
+    printf("%d %d %d", cnt_class, cnt_hall, cnt_bath);
+    return 0;
+}
