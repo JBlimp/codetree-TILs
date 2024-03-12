0298fc93c678f770c3f6e18fef363aa7e3ff79f9
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-12 16:32:11 +0900

    Title: 특정 숫자 맞추기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240312/특정 숫자 맞추기/catching-specific-number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    while (1) {
+        scanf("%d", &n);
+        if (n < 25) {
+            printf("Lower\n");
+        } else if (n > 25) {
+            printf("Higher\n");
+        } else {
+            printf("Good");
+            break;
+        }
+    }
+    return 0;
+}
