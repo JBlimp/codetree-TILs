6bd03678101d9a542313b7323462f5648875e588
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-13 10:18:28 +0900

    Title: 2의 거듭제곱수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240313/2의 거듭제곱수/pow-of-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, x = 0;
+    scanf("%d", &n);
+
+    while (n != 1) {
+        n /= 2;
+        x++;
+    }
+
+    printf("%d", x);
+    return 0;
+}
