c309843a139d64ffe196b70847aeb7f31764a17f
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-03-13 10:26:48 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240313/합성수 판별기/factorization-discriminator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 2; i <= n / 2; i++) {
+        if (n % i == 0) {
+            printf("Y");
+            return 0;
+        }
+    }
+
+    printf("N");
+    return 0;
+}
